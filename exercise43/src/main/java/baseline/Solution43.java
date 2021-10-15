b0f0844ca401457fda3123f43918fe69ec214f25
gabotelle <gabriel.telleria@knights.ucf.edu>
2021-10-15 01:54:44 -0400

    e43

---
+++ exercise43/src/main/java/baseline/Solution43.java
@@ -0,0 +1,27 @@
+package baseline;
+
+/*
+ *  UCF COP3330 Fall 2021 Assignment 4 Solution
+ *  Copyright 2021 Gabriel Telleria
+ */
+
+public class Solution43 {
+    /*
+    Prompt "Site Name:"
+        save into <siteName>
+    Prompt "Author:"
+        save into <author>
+    Prompt "Do you want a folder for JavaScript?"
+        save into <JSFolder>
+    Prompt "Do you want a folder for CSS?"
+        save into <CSSFolder>
+    Create website
+        create index.html
+            Set title <siteName>
+            Set meta author <author>
+            if JsFolder is true
+                Create JavaScript folder
+            if CSSFolder is true
+                Create CSS folder
+     */
+}
