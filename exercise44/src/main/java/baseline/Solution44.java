3e1a5b36b038eccfde2f086c5bbb91f8d448b3dc
gabotelle <gabriel.telleria@knights.ucf.edu>
2021-10-17 15:30:35 -0400

    e44

---
+++ exercise44/src/main/java/baseline/Solution44.java
@@ -0,0 +1,24 @@
+package baseline;
+/*
+ *  UCF COP3330 Fall 2021 Assignment 4 Solutions
+ *  Copyright 2021 first_name last_name
+ */
+
+/*
+Product
+    -Name
+    -Price
+    -quantity
+Read the json file
+save into <products>
+Prompt user for name of product
+save into <productName>
+if <productName> inside <products>
+    Print "Name: <name>
+            Price: <price>
+            Quantity: <quantity>
+else
+    Prompt again
+ */
+public class Solution44 {
+}
