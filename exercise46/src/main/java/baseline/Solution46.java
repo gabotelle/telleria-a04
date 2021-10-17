eb10d487d064968237b119a75e6b764d5628641a
gabotelle <gabriel.telleria@knights.ucf.edu>
2021-10-17 19:56:39 -0400

    e46

---
+++ exercise46/src/main/java/baseline/Solution46.java
@@ -0,0 +1,22 @@
+package baseline;
+/*
+ *  UCF COP3330 Fall 2021 Assignment 4 Solutions
+ *  Copyright 2021 Gabriel Telleria
+ */
+
+public class Solution46 {
+    /*
+    Read the file
+        if <input> is not in <words>
+            add <input> to words with value 1
+        else
+            add 1 to the value of <input> in <words>
+    Print frequency
+        until <words> is empty
+            find most frequent
+                print "<word>"
+                    for <word> value
+                        print "*"
+            remove <word>
+     */
+}
